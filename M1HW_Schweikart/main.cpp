79be81e27bea5bcf291667e5d8e5cf7a43e4f01c
Brian Schweikart <31232164+BSchweikart@users.noreply.github.com>
2019-01-17 14:00:08 -0500

    Intro program

---
+++ M1HW_Schweikart/main.cpp
@@ -0,0 +1,25 @@
+// CSC 134
+// M1HW1
+// Brian Schweikart
+// 01/17/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    Provide a brief introduction
+    using cout.
+    */
+    cout << "Hello, my name is Brian A Schweikart" << endl;
+    cout << "I'm in my final semester for my AA in programming" << endl;
+    cout << "I will be graduating on May " << 17 << "th" << endl;
+    cout << "I have taken " << 3 << " programming languages" << endl;
+    cout << "\t1. C#" << endl;
+    cout << "\t2. Python" << endl;
+    cout << "\t3. basic Java" << endl;
+    cout << "I'm in the process of learning SQL, and C++" << endl;
+    return 0;
+}
